d42b73a86ba3691e1350108c88c5ecbbfa08b94b
sriselva <sriselva231096@gmail.com>
2019-01-29 09:44:31 +0530

    second commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask,request,make_response
+app = Flask(__name__)
+
+@app.route('/set')
+def setcookie():
+    response=make_response("i have created cookies")
+    response.set_cookie("mynewcookie","selvakumar")
+    return response
+
+@app.route('/get')
+def getcookie():
+    cookiess=request.cookies.get("mynewcookie")
+    return "my new cookie is"+cookiess+"is created"
+
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
